58f7dd5c1669792076052b31213857289b13826b
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-25 10:58:56 +1000

    Update Domains with mapping

--- contracts/Domain.sol
+++ contracts/Domain.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
-  constructor() {
-    console.log("THIS IS MY DOPE GOAT DOMAINS CONTRACT.");
-  }
+
+    mapping(string => address) public domains;
+
+    constructor() {
+        console.log("THIS IS MY DOPE GOAT DOMAINS CONTRACT.");
+    }
+
+    // A register function that adds their names to our mapping
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    // This will give us the domain owners' address
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
 }
