3ad543190b395b65ae24879cb68fc15e7d0d0165
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-25 11:18:35 +1000

    Update Comains with records

--- contracts/Domain.sol
+++ contracts/Domain.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 contract Domains {
 
     mapping(string => address) public domains;
+    mapping(string => string) public records;
 
     constructor() {
         console.log("THIS IS MY DOPE GOAT DOMAINS CONTRACT.");
@@ -14,6 +15,9 @@ contract Domains {
 
     // A register function that adds their names to our mapping
     function register(string calldata name) public {
+        // Check if the name is already registered
+        require(domains[name] == address(0)); 
+
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
@@ -21,5 +25,16 @@ contract Domains {
     // This will give us the domain owners' address
     function getAddress(string calldata name) public view returns (address) {
         return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        // Check that the owner is the transaction sender
+        require(domains[name] == msg.sender);
+
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
     }
 }
