1b8be31c0256a4cb2e51cc8f7bf48db8f22c8657
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-25 17:21:26 +1000

    Add Conenct Functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -8,8 +8,32 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
+	//State Varriables
+	const [currentAccount, setCurrentAccount] = useState('');
+
+	// Connect Waller
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert("Get MetaMask -> https://metamask.io/");
+				return;
+			}
+
+			// Fancy method to request access to account.
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+		
+			// Boom! This should print out public address once we authorize Metamask.
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	// Gotta make sure this is async.
-	const checkIfWalletIsConnected = () => {
+	const checkIfWalletIsConnected = async () => {
 		// First make sure we have access to window.ethereum
 		const { ethereum } = window;
 
@@ -19,13 +43,24 @@ const App = () => {
 		} else {
 			console.log("We have the ethereum object", ethereum);
 		}
+
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		// Users can have multiple authorized accounts, we grab the first one if its there!
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account:', account);
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
 	}
 
 	// Create a function to render if wallet is not connected yet
 	const renderNotConnectedContainer = () => (
 		<div className="connect-wallet-container">
 			<img src="https://media.giphy.com/media/cMso9wDwqSy3e/giphy.gif" alt="Goat gif" />
-			<button className="cta-button connect-wallet-button">
+			<button onClick={connectWallet} className="cta-button connect-wallet-button">
 				Connect Wallet
 			</button>
 		</div>
@@ -52,7 +87,7 @@ const App = () => {
 				</div>
 
 				{/* Add your render method here */}
-				{renderNotConnectedContainer()}
+				{!currentAccount && renderNotConnectedContainer()}
 
 				<div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
