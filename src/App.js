44ef37f887f3efe4c820ec46e7f014dbaf3de321
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-25 16:43:26 +1000

    Update web app to be more goat friendly

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'chainedgandalf';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -15,8 +15,8 @@ const App = () => {
 				<div className="header-container">
 					<header>
             <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
+              <p className="title"> 🐐Dope Goat Name Service</p>
+              <p className="subtitle">Your immortal Dope Goat on the blockchain!</p>
             </div>
 					</header>
 				</div>
@@ -28,7 +28,7 @@ const App = () => {
 						href={TWITTER_LINK}
 						target="_blank"
 						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
+					>{`built by @${TWITTER_HANDLE}`}</a>
 				</div>
 			</div>
 		</div>
