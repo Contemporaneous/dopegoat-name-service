e3c2489b1cdca8fcc8188200e7303dd6418b96bb
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-25 11:19:12 +1000

    Update run script with records

--- scripts/run.js
+++ scripts/run.js
@@ -3,15 +3,42 @@ const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
-    
+
     console.log("Contract deployed to:", domainContract.address);
     console.log("Contract deployed by:", owner.address);
     
-    const txn = await domainContract.register("doom");
+    //register
+    let txn = await domainContract.register("doom");
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress("doom");
+    //get owner
+    let domainOwner = await domainContract.getAddress("doom");
     console.log("Owner of domain:", domainOwner);
+
+    //setrecord
+    txn = await domainContract.setRecord("doom","Here is some doom");
+    await txn.wait();
+
+    //getrecord
+    let record = await domainContract.getRecord("doom");
+    console.log("Record of domain:", record);
+
+    //register
+    txn = await domainContract.connect(randomPerson).register("doom2");
+    await txn.wait();
+
+    //get owner
+    domainOwner = await domainContract.connect(randomPerson).getAddress("doom2");
+    console.log("Owner of domain:", domainOwner);
+
+    //setrecord
+    txn = await domainContract.connect(randomPerson).setRecord("doom2","Here is some more doom");
+    await txn.wait();
+
+    //getrecord
+    record = await domainContract.connect(randomPerson).getRecord("doom2");
+    console.log("Record of domain:", record);
+
   };
   
   const runMain = async () => {
