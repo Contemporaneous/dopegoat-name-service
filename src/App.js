811b7829d1ab7cb1732c6b6940c9a24a8181b773
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-02-25 17:14:30 +1000

    Add Wallet Connect Functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -8,27 +8,60 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
-  return (
+	// Gotta make sure this is async.
+	const checkIfWalletIsConnected = () => {
+		// First make sure we have access to window.ethereum
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log("Make sure you have MetaMask!");
+			return;
+		} else {
+			console.log("We have the ethereum object", ethereum);
+		}
+	}
+
+	// Create a function to render if wallet is not connected yet
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img src="https://media.giphy.com/media/cMso9wDwqSy3e/giphy.gif" alt="Goat gif" />
+			<button className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+  	);
+
+	// useEffects 
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, [])
+
+
+
+ 	return (
 		<div className="App">
 			<div className="container">
 
 				<div className="header-container">
 					<header>
-            <div className="left">
-              <p className="title"> 🐐Dope Goat Name Service</p>
-              <p className="subtitle">Your immortal Dope Goat on the blockchain!</p>
-            </div>
+            			<div className="left">
+              				<p className="title"> 🐐Dope Goat Name Service</p>
+              				<p className="subtitle">Your immortal Dope Goat on the blockchain!</p>
+            			</div>
 					</header>
 				</div>
 
-        <div className="footer-container">
+				{/* Add your render method here */}
+				{renderNotConnectedContainer()}
+
+				<div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built by @${TWITTER_HANDLE}`}</a>
+						<a
+							className="footer-text"
+							href={TWITTER_LINK}
+							target="_blank"
+							rel="noreferrer"
+						>{`built by @${TWITTER_HANDLE}`}</a>
 				</div>
 			</div>
 		</div>
