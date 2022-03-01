e4a20e681b7820df2c6423a2443bc9d69617b6ed
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-03-02 08:30:40 +1000

    Update deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,11 @@
+const fs = require("fs");
+const path = require('path');
+
+// source to copy content
+const src = path.join(__dirname, '..', 'artifacts', 'contracts', 'Domain.sol', 'Domains.json');
+// destination for copied content
+const dest = path.join(__dirname,'..','src','utils','contractABI.json');
+
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy("dgtest");
@@ -6,15 +14,17 @@ const main = async () => {
     console.log("Contract deployed to:", domainContract.address);
   
     // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-    let txn = await domainContract.register("origin",  {value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("Origin",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
     console.log("Minted domain origin.dgtest");
   
-    txn = await domainContract.setRecord("origin", "I am the Original Goat");
+    txn = await domainContract.setRecord("Origin","Name", "Origin");
     await txn.wait();
-    console.log("Set record for origin.dgtest");
+    txn = await domainContract.setRecord("Origin","Favourite Food", "Goats");
+    await txn.wait();
+    console.log("Set records for origin.dgtest");
   
-    const address = await domainContract.getAddress("origin");
+    const address = await domainContract.getAddress("Origin");
     console.log("Owner of domain origin:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
@@ -24,6 +34,16 @@ const main = async () => {
 const runMain = async () => {
     try {
         await main();
+        fs.copyFile(src, dest, (error) => {
+            // incase of any error
+            console.log("Lets Copy the ABI file");
+            if (error) {
+              console.error(error);
+              return;
+            }
+            
+            console.log("Copied Successfully!");
+        });
         process.exit(0);
     } catch (error) {
         console.log(error);
